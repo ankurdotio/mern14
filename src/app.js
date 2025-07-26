acc33c944b7ffabcf5a7e71c47371190c4cd570a
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-07-26 16:29:51 +0530

    clean up note fetching and enhance delete/update routes

--- src/app.js
+++ src/app.js
@@ -15,12 +15,35 @@ app.post('/notes', async (req, res) => {
 })
 
 app.get('/notes',async (req,res)=>{
-   const notes = await noteModel.find()
+   const notes = await noteModel.find({   })
    res.status(200).json({
     message: "Notes fetched successfully",
     notes
    })
 })
+
+app.delete("/notes/:id",async (req,res)=>{
+    const id = req.params.id
+    await noteModel.findOneAndDelete({
+        _id: id
+    })
+    res.status(200).json({
+        message: "Note deleted successfully"
+    })
+})
+
+app.patch('/notes/:id',async(req,res)=>{
+
+    const id = req.params.id
+    const {title} = req.body
+    await noteModel.findOneAndUpdate({ _id:id },{title})
+    
+    res.status(200).json({
+        message: "Note updated successfully"
+    })
+})
+
+
 
 
 
