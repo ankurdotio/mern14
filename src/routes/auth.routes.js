87f5570beef2cd0cd8971f6d63d2839becae82bc
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-08-18 16:44:09 +0530

    Refactor authentication routes: improve response status codes and enhance login error handling

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,34 +3,54 @@ const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
+router.post("/register", async (req, res) => {
 
-/* POST /api/auth/register */
-router.post('/register', async (req, res) => {
     const { email, password } = req.body;
     const isUserAlreadyExists = await userModel.findOne({ email })
+
     if (isUserAlreadyExists) {
-        return res.status(400).json({
+        return res.status(409).json({
             message: "User already exists"
         })
     }
 
-    const user = await userModel.create({
-        email, password
-    })
+    const user = await userModel.create({ email, password })
 
-    const token = jwt.sign({
-        email: user.email,
-        id: user._id
-    }, "7e435152c166330df2e0e62c8e42881f")
+    const token = jwt.sign({ id: user._id }, "3d02e3016491447b9aa9d0966726c331")
 
     res.status(201).json({
-        message: "User created successfully",
+        message: "user registered successfully",
         user,
         token
     })
 
 })
 
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        res.status(404).json({
+            message: "User not found"
+        })
+    }
+    const isPasswordValid = password === user.password;
+
+    if (!isPasswordValid) {
+        return res.status(401).json({
+            message: "Invalid password"
+        })
+    }
+
+    const token = jwt.sign({ id: user._id }, "3d02e3016491447b9aa9d0966726c331")
+
+    res.status(200).json({
+        message: "Login successful",
+        user,
+        token
+    })
+})
 
 
 module.exports = router;
