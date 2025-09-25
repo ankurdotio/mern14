90bb0cbdea25ff2ac18189eb224971716336dcb3
Ankur Prajapati <91510065+ankurdotio@users.noreply.github.com>
2025-09-25 13:50:54 +0530

    Remove middleware for JSON and cookie parsing

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const productRoutes = require("./routes/product.routes")
 
 const app = express()
 
-app.use(express.json())
-app.use(cookieParser())
+
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
